49ca23c28e54af5dadd48a53a6987b17caf22ff4
Dabaz420 <73653156+Dabaz420@users.noreply.github.com>
2020-11-25 09:02:01 +0100

    Update account-create--process.php

--- account-create--process.php
+++ account-create--process.php
@@ -1,9 +1,42 @@
 <?php
 
   $userNom = $_POST["user-nom"] ?? false;
+  $userPrenom = $_POST["user-prenom"] ?? false;
+  $userAdresse = $_POST["user-adresse"] ?? false;
+  $userLogin = $_POST["user-login"] ?? false;
+  $userPsw = $_POST["user-password"] ?? false;
+  $userCP = $_POST["user-cp"] ?? false;
+  $userVille = $_POST["user-ville"] ?? false;
+  $userPays = $_POST["user-pays"] ?? false; 
+  $userTel = $_POST["user-tel"] ?? false;
+  
+  $destinataire = $userLogin;
+  $envoyeur	="ne_pas_repondre@AlainAckerman.com";
+  $sujet = "Votre identifiant de connexion au site d’Alain Ackerman";
+  $message = "Bonjour " . $userPrenom . " " . $userNom . "\r\n" .
 
-?><!DOCTYPE html>
-<html lang="en">
+             "Nous vous remercions d'avoir créé votre compte client et vous souhaitons la bienvenue sur le site d'Alain Ackerman. \r\n" .
+
+             "Vos données de connexion vous permettent d'accéder à l'intégralité du site. Vous pouvez à tout moment gérer les données de votre compte (https://alainackerman.com/account)." .
+
+             "Meilleures salutations.\r\n" .
+             "L’équipe du site Alain Ackerman \r\n";
+
+  $headers = 'From: '.$envoyeur . "\r\n" .
+     		 'Reply-To: '.$envoyeur. "\r\n" .
+     		 'X-Mailer: PHP/' . phpversion();
+  $envoye = mail($destinataire, $sujet, $message, $headers);
+  if ($envoye){
+  echo "<br />Email envoyé.";
+  }
+  else{
+  echo "<br />Email refusé.";
+  }
+  
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,14 +48,14 @@
     <p>Bien le bonjour, je suis le fichier <code>account-create--process.php</code> et voici les valeurs que je viens tout juste de recevoir par la méthode <code>POST</code>:</p>
     <ul>
       <li>Nom : <?php echo $userNom; ?></li>
-      <li>Prénom :</li>
-      <li>Adresse :</li>
-      <li>Code postal :</li>
-      <li>Ville :</li>
-      <li>Pays :</li>
-      <li>Téléphone :</li>
-      <li>Adresse électronique :</li>
-      <li>Mot de passe :</li>
+      <li>Prénom : <?php echo $userPrenom; ?></li>
+      <li>Adresse : <?php echo $userAdresse; ?></li>
+      <li>Code postal : <?php echo $userCP; ?></li>
+      <li>Ville : <?php echo $userVille; ?></li>
+      <li>Pays : <?php echo $userPays; ?></li>
+      <li>Téléphone : <?php echo $userTel; ?></li>
+      <li>Adresse électronique : <?php echo $userLogin; ?></li>
+      <li>Mot de passe : <?php echo $userPsw; ?></li>
     </ul>
   </main>
 </body>
